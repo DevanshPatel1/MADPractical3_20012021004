9aaedf358b046872e537b0307afe90eb7360e9d4
Student <neehopa@gmail.com>
2022-09-12 11:16:45 +0530

    Practical 3 Started

---
+++ app/src/main/java/com/example/madpractical3_20012021004/RegistrationActivity.kt
@@ -0,0 +1,18 @@
+package com.example.madpractical3_20012021004
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.view.WindowCompat
+import com.example.madpractical3_20012021004.databinding.ActivityMainBinding
+import com.example.madpractical3_20012021004.databinding.ActivityRegistrationBinding
+
+class RegistrationActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegistrationBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        super.onCreate(savedInstanceState)
+        binding = ActivityRegistrationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+    }
+}
